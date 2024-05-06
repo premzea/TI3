919e499f2a6c1eccd4140a15f1fd7ad86d96447e
premzea <1209premzea@gmail.com>
2024-05-06 08:33:04 -0500

    commit

---
+++ src/model/Project.java
@@ -0,0 +1,10 @@
+//Project.java
+
+import java.util.Calendar;
+
+public abstract class Project{
+    protected String code;
+    protected String name;
+    protected Calendar dateClassification = Calendar.getInstance();
+    // dateClassification.set(Calendar.YEAR,"2023");
+}
---
+++ src/model/ProjectType.java
@@ -0,0 +1,6 @@
+//ProjectType.java
+
+public enum ProjectType{
+    KNOLEDGE_MANAGEMENT,
+    TRANSFORMATION_AND_OPTIMIZATION;
+}
